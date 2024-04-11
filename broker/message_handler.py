f82928f723aceaf887bae8d4bb4bff42ae7234f1
Laurenz Dilba <laurenz.dilba@hotmail.de>
2024-04-11 21:31:51 +0200

    fixed error response

--- broker/message_handler.py
+++ broker/message_handler.py
@@ -1,6 +1,7 @@
 import json
 
 from broker.response_generator import respond
+from logic import fsm_handler
 from logic.command_handler import handle_command
 from broker import gateway
 
@@ -19,13 +20,17 @@ async def handle(command, id):
             else:
                 res = await handle_command(command, id)
                 if not res:
+                    data = {'order': fsm_handler.order_flightstrips[command['args'][1]], 'data': dict(fsm_handler.rooms[command['args'][1]])}
+                    del data['data']['password']
                     await gateway.clients[id]["websocket"].send(respond('error',
-                                                                        ['Command has wrong format', gateway.rooms[command[1]]]))
+                                                                        ['Command not accepted', data]))
         except Exception as e:
-            await gateway.clients[id]["websocket"].send(respond('error', ['Command caused an exception',
-                                                                          gateway.rooms[command[1]]]))
+            data = {'order': fsm_handler.order_flightstrips[command['args'][1]], 'data': dict(fsm_handler.rooms[command['args'][1]])}
+            del data['data']['password']
+            await gateway.clients[id]["websocket"].send(respond('error', ['Command caused an exception', data]))
+
     except Exception as e:
-        await gateway.clients[id]["websocket"].send(respond('error', ['Command has wrong format']))
+        await gateway.clients[id]["websocket"].send(respond('error', ['Command has wrong format', str(e)]))
 
 
 async def broadcast(room_id, msg):
