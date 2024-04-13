8451a3097c3042210d80096db726f81293155f84
Laurenz Dilba <laurenz.dilba@hotmail.de>
2024-04-13 12:34:50 +0200

    data get route

--- routes/management.py
+++ routes/management.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from api import response_generator as r, auth
 from broker import gateway
 from logic import fsm_handler
+from logic.fsm_handler import order_flightstrips
 from util import uid
 
 management = Blueprint('management', __name__, url_prefix='/api/v1')
@@ -29,4 +30,17 @@ def create_room_id(room_id):
 
 @management.route('/room/<room_id>', methods=["GET"])
 def get_room_id(room_id):
-    return r.respond({"exists": True})
+    exists = room_id in fsm_handler.rooms
+    return r.respond({"exists": exists})
+
+
+@management.route('/data', methods=["GET"])
+def get_data():
+    res = []
+    for i in fsm_handler.rooms:
+        data = {'order': order_flightstrips[i], 'data': fsm_handler.rooms[i]}
+        del data['data']['password']
+        res.append(data)
+
+    return r.respond(
+        {"data": res, "order_flightstrips": fsm_handler.order_flightstrips, "gatewayRooms": gateway.rooms})
