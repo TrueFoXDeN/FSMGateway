cd642e72a20ab7d9176f203832148c390eee76c9
Laurenz Dilba <laurenz.dilba@hotmail.de>
2024-04-16 17:41:51 +0200

    fixed error

--- api/response_generator.py
+++ api/response_generator.py
@@ -2,13 +2,8 @@ import json
 
 from flask import Response
 
-def respond(r={"success": True}, status=200, json_dump=True, cookie=""):
-    # headers = {"Cache-Control": "no-cache, no-store, must-revalidate, public, max-age=0",
-    #            "Pragma": "no-cache",
-    #            "Expires": "0",
-    #            "Access-Control-Allow-Origin": "*"}
-    # if cookie != "":
-    #     headers['Set-Cookie'] = cookie
+
+def respond(r={"success": True}, status=200, json_dump=True):
     if json_dump:
-        return Response(json.dumps(r), status=status, mimetype='application/json', headers=headers)
-    return Response(r, status=status, mimetype='application/json', headers=headers)
+        return Response(json.dumps(r), status=status, mimetype='application/json')
+    return Response(r, status=status, mimetype='application/json')
